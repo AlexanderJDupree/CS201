86edac75f3458331960d07785df31f7a7b7c36cb
alexanderjdupree <alexander.j.dupree@gmail.com>
2019-01-28 14:32:48 -0800

    Updated quiz snippets

--- snippets/quiz1.c
+++ snippets/quiz1.c
@@ -1,16 +1,19 @@
+
 #include <stdio.h>
 #include <stdint.h>
 
-int main(int argc, char** argv)
+int main()
 {
+    // question 3
+    uint32_t num = 0xa & ~0;
+
+    printf("\n%d\n", num);
+
+    // question 4
     uint32_t i = 0x12345678;
     uint8_t* cp = (uint8_t*) &i;
 
     printf("\n0x%x\n", *cp);
-
-
-    uint32_t num = 0xa & ~0;
-
-    printf("\n%d\n", num);
+    
     return 0;
 }
